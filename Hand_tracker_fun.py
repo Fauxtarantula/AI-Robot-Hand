dab12ca4744ba7d00154cd3184ecc061b89596ba
Muhammad Rakin <68525714+Fauxtarantula@users.noreply.github.com>
2022-05-28 23:47:35 +0800

    Update Hand_tracker_fun.py

--- Hand_tracker_fun.py
+++ Hand_tracker_fun.py
@@ -34,9 +34,8 @@ def hand_type(index, hands, results, ht, wd): #index = hand results(1,0) or left
 
 def get_finger_angle(image, results, jt_arr):
     
-    for hand in results.multi_hand_landmarks:
-        
-        for jt in jt_arr:
+    for hand in results.multi_hand_landmarks:  #going through all hands
+        for jt in jt_arr: #going through all the joints in the joint array
             jt_1 = np.array([hand.landmark[jt[0]].x, hand.landmark[jt[0]].y]) #top joint coord
             jt_2 = np.array([hand.landmark[jt[1]].x, hand.landmark[jt[1]].y]) #middle joint coord
             jt_3 = np.array([hand.landmark[jt[2]].x, hand.landmark[jt[2]].y]) #bottom joint coord
